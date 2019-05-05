4aeaa56180293649be736e927ae4a4da5887086e
Rafles29 <rwozniak@intelgraf.com.pl>
2019-05-05 15:59:24 +0200

    imported mdb for home module and fixed icon in login and register

--- szaklon/src/app/account/account.module.ts
+++ szaklon/src/app/account/account.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ProfileComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MDBBootstrapModulesPro
   ]
 })
 export class AccountModule { }
--- szaklon/src/app/home/home.module.ts
+++ szaklon/src/app/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
 
 @NgModule({
   declarations: [HomepageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MDBBootstrapModulesPro
   ]
 })
 export class HomeModule { }
