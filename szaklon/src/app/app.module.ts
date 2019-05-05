f0cd700c0b7e2e0d485ba8005e0267aaab408c2a
Rafles29 <rwozniak@intelgraf.com.pl>
2019-05-05 14:59:55 +0200

    added core modules and components to project

--- szaklon/src/app/app.module.ts
+++ szaklon/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
 import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 
 import { AppComponent } from './app.component';
+import { AccountModule } from './account/account.module';
+import { HomeModule } from './home/home.module';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     MDBBootstrapModulesPro.forRoot(),
+    AccountModule,
+    HomeModule,
+    NavigationModule,
   ],
   providers: [
     MDBSpinningPreloader
