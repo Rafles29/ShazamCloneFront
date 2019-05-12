f60ab9dcb299dc5435ca0498ba7cac3fe65c1804
Rafał Woźniak <rwozniak@intelgraf.com.pl>
2019-05-12 19:30:40 +0200

    partially working audio input

--- szaklon/src/app/shared/components/audio-input/audio-input.component.ts
+++ szaklon/src/app/shared/components/audio-input/audio-input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,  ViewChild, ElementRef } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
+
 @Component({
   selector: 'app-audio-input',
   templateUrl: './audio-input.component.html',
@@ -8,16 +9,41 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 })
 export class AudioInputComponent implements OnInit {
 
-  @ViewChild('audio')
-  public audio: ElementRef;
+  @ViewChild('audio') audio: ElementRef;
 
   public source: SafeUrl = '';
   public message: string;
+  stream: MediaStream;
+  mediaRecorder: MediaRecorder;
+  audioChunks: any[];
+  recording: boolean = false;
+  blob: Blob; //data for server
 
 
   constructor(private _sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    navigator.mediaDevices.getUserMedia({ audio: true})
+    .then(stream => {
+      this.stream = stream;
+      this.mediaRecorder = new MediaRecorder(stream);
+      this.mediaRecorder.ondataavailable = e => { 
+        if(this.mediaRecorder.state ==  'inactive') {
+          this.blob = e.data;  
+          const reader = new FileReader();
+          reader.readAsDataURL(this.blob);
+          reader.onload = (_event) => {
+            const result = reader.result.toString();      
+            const sanitizeUrl = this._sanitizer.bypassSecurityTrustUrl(result);
+            this.source = sanitizeUrl;
+            console.log(this.source);
+          }
+        }
+      }
+    })
+    .catch(function(err) {
+      console.log(err.message);
+    });
   }
 
 
@@ -43,25 +69,15 @@ export class AudioInputComponent implements OnInit {
   }
 
   public recordAudio() {
-
-    navigator.mediaDevices.enumerateDevices()
-    .then(function(devices) {
-      devices.forEach(function(device) {
-        console.log(device.kind + ": " + device.label +
-                    " id = " + device.deviceId);
-      });
-    })
-    .catch(function(err) {
-      console.log(err.name + ": " + err.message);
-    });
-
-    navigator.mediaDevices.getUserMedia({ audio: true})
-    .then(function(stream) {
-      console.log(stream);
-    })
-    .catch(function(err) {
-      console.log(err.message);
-    });
-}
+    if(this.mediaRecorder.state =='recording') {
+      this.mediaRecorder.stop();
+      console.log('stop');
+      
+    }
+    else {
+      this.mediaRecorder.start();
+      console.log('start');      
+    }
+  }
 
 }
