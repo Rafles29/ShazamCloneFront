928451f43bd9f83e41019b539e2e268bd737058c
Rafles29 <rwozniak@intelgraf.com.pl>
2019-05-12 15:28:42 +0200

    added audio input but microphone recording  doesnt work

--- szaklon/src/app/shared/shared.module.ts
+++ szaklon/src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SongListComponent } from './components/song-list/song-list.component';
 import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { AudioInputComponent } from './components/audio-input/audio-input.component';
 
 @NgModule({
   declarations: [
-    SongListComponent
+    SongListComponent,
+    AudioInputComponent
   ],
   imports: [
     CommonModule,
     MDBBootstrapModulesPro
   ],
-  exports: [SongListComponent]
+  exports: [SongListComponent,
+  AudioInputComponent]
 })
 export class SharedModule { }
